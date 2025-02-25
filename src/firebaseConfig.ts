009097636b372707dec5fcbcd3310fa0be98ceaf
Chris Scribner <chris.scribner1@gmail.com>
2025-02-25 09:35:27 -0400

    Secured API Key

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBPDLpuHUF9eC1IBqJqt8c_OoZvfu4I16E",
-  authDomain: "clear-notes-36d0b.firebaseapp.com",
-  projectId: "clear-notes-36d0b",
-  storageBucket: "clear-notes-36d0b.firebasestorage.app",
-  messagingSenderId: "816072451551",
-  appId: "1:816072451551:web:55a0583d02786f5fdd906a",
-  measurementId: "G-VPBQ219004"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
