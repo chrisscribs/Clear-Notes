06ee9aa1e53716ab378fa8eb586c407f19c065c1
Chris Scribner <chris.scribner1@gmail.com>
2025-03-25 16:46:36 -0300

    Styling login screen

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -6,6 +6,7 @@ import {
   signInWithPopup,
   GoogleAuthProvider,
 } from "firebase/auth";
+import clearnotesLogo from "../assets/clearnotes_logo.svg";
 
 const Login = () => {
   const [email, setEmail] = useState("");
@@ -38,48 +39,58 @@ const Login = () => {
   };
 
   return (
-    <div className="p-4 max-w-md mx-auto mt-20 bg-white rounded shadow">
-      <h2 className="text-xl mb-4 text-center">
-        {isLogin ? "Login" : "Create Account"}
-      </h2>
-      <input
-        className="block w-full p-2 mb-2 border border-gray-700 rounded focus:outline-none focus:ring-2 focus:ring-teal-200"
-        type="email"
-        placeholder="Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <input
-        className="block w-full p-2 mb-2 border border-gray-700 rounded focus:outline-none focus:ring-2 focus:ring-teal-200"
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      {error && <p className="text-red-500">{error}</p>}
-      <button
-        className="bg-green-600 text-white px-4 py-2 rounded w-full hover:bg-green-400"
-        onClick={handleSubmit}
-      >
-        {isLogin ? "Login" : "Sign Up"}
-      </button>
-      <button
-        className="text-sm text-teal-500 mt-2 underline"
-        onClick={() => setIsLogin(!isLogin)}
-      >
-        {isLogin ? "Don't have an account? Sign up" : "Have an account? Login"}
-      </button>
-      <button
-        className="bg-white border text-gray-700 px-4 py-2 rounded w-full flex items-center justify-center gap-2 mt-2 shadow hover:bg-gray-50"
-        onClick={handleGoogleLogin}
-      >
+    <div className="flex flex-col justify-center w-full min-h-screen bg-green-500">
+      <div className="py-4 px-12 w-full max-w-md mx-auto bg-white rounded-lg shadow">
         <img
-          src="https://www.gstatic.com/firebasejs/ui/2.0.0/images/auth/google.svg"
-          alt="Google logo"
-          className="w-5 h-5"
+          src={clearnotesLogo}
+          alt="clearnotesLogo"
+          width={80}
+          className="block mx-auto mb-4"
         />
-        Sign in with Google
-      </button>
+        <h2 className="text-xl mb-4 text-center">
+          {isLogin ? "Login" : "Create Account"}
+        </h2>
+        <input
+          className="block w-full p-2 mb-4 border border-gray-700 rounded focus:outline-none focus:ring-2 focus:ring-teal-200"
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          className="block w-full p-2 mb-4 border border-gray-700 rounded focus:outline-none focus:ring-2 focus:ring-teal-200"
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        {error && <p className="text-red-500">{error}</p>}
+        <button
+          className="bg-green-600 text-white px-4 py-2 mb-2 rounded w-full hover:bg-green-400"
+          onClick={handleSubmit}
+        >
+          {isLogin ? "Login" : "Sign Up"}
+        </button>
+        <button
+          className="text-sm text-teal-500 mb-4 underline"
+          onClick={() => setIsLogin(!isLogin)}
+        >
+          {isLogin
+            ? "Don't have an account? Sign up"
+            : "Have an account? Login"}
+        </button>
+        <button
+          className="bg-white border mb-4 text-gray-700 px-4 py-2 rounded w-full flex items-center justify-center gap-2 mt-2 shadow hover:bg-gray-50"
+          onClick={handleGoogleLogin}
+        >
+          <img
+            src="https://www.gstatic.com/firebasejs/ui/2.0.0/images/auth/google.svg"
+            alt="Google logo"
+            className="w-5 h-5"
+          />
+          Sign in with Google
+        </button>
+      </div>
     </div>
   );
 };
